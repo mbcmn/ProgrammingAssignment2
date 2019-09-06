1ae2a27d5c1f144e148fd897db589b23a528389c
mbcnm <42297182+mbcmn@users.noreply.github.com>
2019-09-06 15:22:54 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+###and whether the comments you include in your functions indicate that you understand
+###lexical scoping and the superassignment operator (<<-) 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+    
 }
+
+### add comments for each function to explain understanding
+
+## whether the comments you include in your functions indicate that you understand
+## lexical scoping and the superassignment operator (<<-) as used in these functions
+
+### Commands
+###
+### a <- makeCacheMatrix(matrix(1:4, 2, 2))
+### cachemean(a)
+
+
